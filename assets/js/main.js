4ec47840a6a4f136c9d6cd5bf6779459bbaf5f53
wy <932051163@qq.com>
2017-09-02 00:42:36 +0800

    second

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,9 +20,45 @@ $(function() {
                 i = 1;
             }
             $('.flier .flier-box').animate({
-                top: -itemH * i + 1 + 'px'
+                top: -itemH * i + 'px'
             }, 400, 'ease');
         }
     }
+
+    $('.touch').singleTap(function() {
+
+        gotommd($($(this).attr('data-mmd')).offset().top);
+        $(this).parent('li').addClass('on').siblings().removeClass('on');
+        return false;
+    });
+
+    $('#gototop').singleTap(function() {
+        gotommd(0);
+    });
+
+    /*发现zepto的animate()源码采用css3的方式进行，而scrollTop属性不在css3的动画属性中，所以没有生效。接下来的方法就是自己写一个滚动条上下滚动的方法。初步的代码如下*/
+    function gotommd(mdd) {
+        var gotop = null,
+            wt = $(window).scrollTop(),
+            cha = wt - mdd,
+            i = 0,
+            duration = 300,
+            num = 5,
+            avg = cha / duration * num,
+            st;
+        move();
+
+        function move() {
+            gotop = setInterval(function() {
+                i++;
+                st = wt - avg * i;
+                $(window).scrollTop(st);
+                if (i == duration / num) {
+                    clearInterval(gotop);
+                }
+            }, 1);
+        }
+    }
+
 
 });
